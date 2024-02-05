dd108c7d09098267b246968bef235f9029e9afef
JesusG <py3user22@github.com>
2024-02-05 00:34:36 -0600

    0205 restart save1

---
+++ b_app206/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BApp206Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'b_app206'
---
+++ b_app206/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home),
+    path('home', views.home, name="home"),
+
+
+    #203 menu urls add.
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
+
+    #203 header links add
+    path('about/', views.about, name="about"),
+
+    #203 book reservations add
+    path('book/', views.book, name="book"),
+    path('bookings/', views.bookings, name="bookings"),
+    path('reservations/', views.bookings, name="reservations"),
+
+
+
+]
